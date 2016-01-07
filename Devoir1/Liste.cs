d325bcee008d04226e88a5fe237fe8320f48c25e
Francis <super_f_qc@hotmail.com>
2016-01-06 19:55:22 -0500

    Init + implementation (inverser ne fonctionne pas)

---
+++ Devoir1/Liste.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devoir1
+{
+    class Liste<T> where T :new()
+    {
+        class Noeud<U> where U :T, new()
+        {
+            public Noeud<U> succ;
+
+            public U Val;
+
+            public Noeud()
+            {
+                Val = new U();
+                succ = null;
+            }
+
+            public Noeud(U val)
+            {
+                Val = val;
+                succ = null;
+            }
+
+
+        }
+
+        private Noeud<T> tete;
+        private Noeud<T> dernier;
+
+        public Liste()
+        {
+            tete = null;
+            dernier = null;
+        }
+
+        public Liste(Liste<T> autre )
+        {
+
+        }
+
+        public bool est_vide()
+        {
+            return tete == null;
+        }
+
+        public void ajouter(T val)
+        {
+            if(est_vide())
+            {
+                tete = dernier = new Noeud<T>(val);
+            }
+            else
+            {
+                dernier.succ = new Noeud<T>(val);
+                dernier = dernier.succ;
+            }
+        }
+
+        public T extraire()
+        {
+            if (est_vide()) throw new Exception("Vide");
+            Noeud<T> temp = tete;
+            tete = tete.succ;
+            return temp.Val;
+        } 
+
+        public int taille()
+        {
+            int n = 0;
+            for (Noeud<T> i = tete; i != null; i = i.succ)
+            {
+                n++;
+            }
+            return n;
+        }
+
+        public void inverser()
+        {
+            Noeud<T> nouvelleTete=new Noeud<T>();
+            while(tete!=null)
+            {
+                Noeud<T> temp = new Noeud<T>(tete.Val);
+                temp.succ = nouvelleTete;
+                temp = tete;
+                tete = temp.succ;
+            }
+            tete = nouvelleTete;
+        }
+    }
+}
---
+++ Devoir1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devoir1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Liste<int> lst = new Liste<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                lst.ajouter(i + 1);
+            }
+
+            Liste<int> rlst = new Liste<int>(lst);
+            rlst.inverser();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(rlst.extraire());
+            }
+        }
+    }
+}
